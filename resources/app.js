1856adfe809aacff4bd3571a71c18444e5859fda
aindriu80 <aindriu80@gmail.com>
2019-02-08 20:51:29 +0000

    Save location to local storage

--- resources/app.js
+++ resources/app.js
@@ -1,10 +1,33 @@
+// Init storage
+const storage = new Storage();
+
+//Get Stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather("Miami", "US");
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Init UI
 // const ui = new UI();
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", e => {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("city").value;
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $("#locModal").modal("hide");
+});
+
+// Chage Weather
+weather.changeLocation(city, state);
+// Set Location in LS
+storage.setLocationData(city, state);
 
 function getWeather() {
   weather
